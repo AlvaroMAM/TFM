b6066bba3301b1f3d388bbcc352d49f883255558
Alvaro Manuel <alvaroapariciomorales@gmail.com>
2024-04-09 12:30:19 +0200

    Modifications of name and creating information processing process

--- code/1_InformationProcessing/information_processing.py
+++ code/1_InformationProcessing/information_processing.py
@@ -7,5 +7,25 @@ Descripcion:
 Proceso encargado de procesar la información de los archivos de especificaciones
 y distribuirla a los servicios (QPU_Selector o CPU_Selector) según corresponda.
 """
+from flask import Flask
+import requests
 
-import Flask
+
+FOLDER_NAME = "deployment_app_folder"
+
+app = Flask(__name__)
+
+
+@app.route('/')
+# Function to describe the status of the service
+def index():
+    return "ONLINE"
+
+
+@app.route('/start')
+def start_processing():
+    # leer archivo .zip
+    # descomprimir en carpeta máquina local
+    # leer archivos principales
+    # leer archivos de requirements
+    return None
