bf9cf312c1f84c9b01c5eb3ec216c051ac0abffb
Alvaro Manuel <alvaroapariciomorales@gmail.com>
2024-04-10 16:37:12 +0200

    Creation of yaml files for cloud providers

--- code/2_CPUSelector/cpu_selector.py
+++ code/2_CPUSelector/cpu_selector.py
@@ -12,7 +12,19 @@ from ..config.config import KAFKA_SERVER_URL, TOPIC_CPU, TOPIC_CPU_CANDIDATES
 import json
 import logging
 """
-JSON SALIDA
+# JSON ENTRADA
+{
+file_name : {
+    id : x,
+    mode : y,
+    requests: z,
+    execution_time: h,
+    ram: j,
+    cpu: k
+    }
+}
+
+# JSON SALIDA
 app : {
         microservice_1: {
             cpu_machines : [],
@@ -24,10 +36,41 @@ app : {
     }
 """
 
+def select_cpu (requests, execution_time, cpu, ram):
+    """
+    # Leer archivo con características de las máquinas
+    # Iterar por cada máquina, en cada iteración comprobar características si es compatible/candidata
+    # Si lo es, añadir diccionario a lista
+    RETURN FORMAT 
+    selected_cpus = [
+        machine_1 : {
+        characteristic_1 : value,
+        characteristic_2 : value,
+        ...
+        },
+        machine_2 : {
+        characteristic_1 : value,
+        characteristic_2 : value,
+        ...
+        }
+        ...
+    ]
+    
+    """
+    selected_cpus = []
+    cloud_providers = ""
+    return selected_cpus
+
 if __name__ == '__main__':
     consumer = KafkaConsumer(TOPIC_CPU, bootstrap_servers=[KAFKA_SERVER_URL], value_serializer=lambda x: json.loads(x).encode('utf-8')) # CREATING KAFKA CONSUMER
     producer = KafkaProducer(bootstrap_servers=[KAFKA_SERVER_URL], value_serializer=lambda x: json.dumps(x).encode('utf-8')) # CREATING KAFKA PRODUCER
-    logging.basicConfig(filename='cpu-selector.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p') #CREATING LOGGING CONFIGURATION
+    logging.basicConfig(filename='cpu-selector.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p') # CREATING LOGGING CONFIGURATION
     app_cpu_machines = dict()
     for message in consumer:
-        print(message)
+        logging.debug("CPU-SELECTOR : MESSAGE RECIEVED")
+        microservices = message.value
+
+        for microservice_name, requirements in microservices.items():
+            logging.debug("CPU-SELECTOR : PROCESSING MICROSERVICE" + microservice_name)
+            app_cpu_machines[microservice_name] = select_cpu(requirements['requests'], requirements['execution_time'], requirements['cpu'], requirements['ram']) # Returns an Array<Dict> of the suitable CPUs machines from AWS
+
--- code/2_QPUSelector/qpu_selector.py
+++ code/2_QPUSelector/qpu_selector.py
@@ -7,6 +7,10 @@ Descripcion:
 Servicio encargado de seleccionar un conjunto de máquinas de QPU compatibles
 para la ejecución de los microservicios que conforman la aplicación híbrida (cuántico-clásica)
 """
+from kafka import KafkaConsumer, KafkaProducer
+from ..config.config import KAFKA_SERVER_URL, TOPIC_QPU, TOPIC_QPU_CANDIDATES
+import json
+import logging
 
 """
 JSON SALIDA
@@ -22,3 +26,38 @@ app : {
     }
 
 """
+
+def select_qpu(qubits, shots):
+    """
+    RETURN FORMAT 
+    selected_qpus = [
+        machine_1 : {
+        characteristic_1 : value,
+        characteristic_2 : value,
+        ...
+        },
+        machine_2 : {
+        characteristic_1 : value,
+        characteristic_2 : value,
+        ...
+        }
+        ...
+    ]
+    
+    """
+    selected_qpus = []
+    return selected_qpus
+
+if __name__ == '__main__':
+    consumer = KafkaConsumer(TOPIC_QPU, bootstrap_servers=[KAFKA_SERVER_URL], value_serializer=lambda x: json.loads(x).encode('utf-8')) # CREATING KAFKA CONSUMER
+    producer = KafkaProducer(bootstrap_servers=[KAFKA_SERVER_URL], value_serializer=lambda x: json.dumps(x).encode('utf-8')) # CREATING KAFKA PRODUCER
+    logging.basicConfig(filename='qpu-selector.log', encoding='utf-8', level=logging.DEBUG, format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p') # CREATING LOGGING CONFIGURATION
+    app_qpu_machines = dict()
+    for message in consumer:
+        logging.debug("QPU-SELECTOR : MESSAGE RECIEVED")
+        microservices = message.value
+
+        for microservice_name, requirements in microservices.items():
+            logging.debug("QPU-SELECTOR : PROCESSING MICROSERVICE" + microservice_name)
+            app_qpu_machines[microservice_name] = select_qpu(requirements['qubits'], requirements['shots']) # Returns an Array<Dict> of the suitable CPUs machines from AWs
+
