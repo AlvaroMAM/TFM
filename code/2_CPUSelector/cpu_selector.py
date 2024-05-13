8b77b6da5bee5ceb1f3d2ae67238f3707851de5c
AlvaroMAMunex <amapamor@unex.es>
2024-05-13 22:34:02 +0200

    prueba

--- code/2_CPUSelector/cpu_selector.py
+++ code/2_CPUSelector/cpu_selector.py
@@ -114,10 +114,11 @@ if __name__ == '__main__':
     print("CPU INIT")
     for message in consumer:
         print("CPU RECIEVED")
-        """
+        
         logging.debug("CPU-SELECTOR : MESSAGE RECIEVED")
         microservices = json.loads(message.value.decode('utf-8'))
-
+        print(microservices)
+        """
         for microservice_name, requirements in microservices.items():
             logging.debug("CPU-SELECTOR : PROCESSING MICROSERVICE:" + microservice_name)
             app_cpu_machines[microservice_name] = select_cpu( requirements['cpu'], requirements['ram'], requirements['number_requests'], requirements['maximum_request_size'], requirements['execution_time'], requirements['availability']) # Returns an Array<Dict> of the suitable CPUs machines from AWS
