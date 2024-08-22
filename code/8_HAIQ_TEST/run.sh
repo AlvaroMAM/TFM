b28e090bfc0b5e7e71451d60c598bad7bf1c3d84
AlvaroMAMunex <amapamor@unex.es>
2024-08-22 23:12:16 +0200

    Testing

---
+++ code/8_HAIQ_TEST/run.sh
@@ -0,0 +1,5 @@
+echo "Running QC_APP - Quantum Classical App)"
+echo "---------------------------------------------------------------"
+../../haiq-analyzer/dist-bin/haiq -model[model.haiq] -properties[0,1]
+echo "---------------------------------------------------------------"
+echo "200"
